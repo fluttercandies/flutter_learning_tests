941285bd7ed01af28249b19970a6f6fb44b5724c
吕彦 <lvyan@lvyandeMacBook-Pro.local>
2020-12-10 16:20:04 +0800

    fix according to review

--- 2 - 百格齐放 - 20201202/lvyan_main.dart
+++ 2 - 百格齐放 - 20201202/lvyan_main.dart
@@ -50,11 +50,52 @@ class MyHomePage extends StatelessWidget {
             .toList(),
       );
 
-  Widget _buildGrid(double width, double left, double top) => Positioned(
+  Widget get _secondPart => LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          final List<Widget> grids = <Widget>[];
+          final List<Widget> mirrorGrids = <Widget>[];
+
+          final double maxWidth = constraints.maxWidth;
+          final double maxHeight = constraints.maxHeight;
+          final double width = (maxWidth / (_gridCount + 1)) * 2;
+          final double height = (maxHeight / (_gridCount + 1)) * 2;
+
+          for (int i = 0; i < _gridCount; i++) {
+            final double offsetX = i * width / 2;
+            final double offsetY = i * height / 2;
+
+            final Widget grid = _buildGrid(
+              width,
+              height,
+              offsetX,
+              offsetY,
+            );
+            grids.add(grid);
+
+            final Widget mirrorGrid = _buildGrid(
+              width,
+              height,
+              maxWidth - offsetX - width,
+              offsetY,
+            );
+            mirrorGrids.add(mirrorGrid);
+          }
+
+          return Stack(children: <Widget>[...grids, ...mirrorGrids]);
+        },
+      );
+
+  Widget _buildGrid(
+    double width,
+    double height,
+    double left,
+    double top,
+  ) =>
+      Positioned(
         left: left,
         top: top,
         width: width,
-        height: width,
+        height: height,
         child: ColoredBox(color: RandomColor.getColor()),
       );
 
@@ -64,36 +105,20 @@ class MyHomePage extends StatelessWidget {
           builder: (_, Orientation orientation) {
             final List<Widget> children = <Widget>[
               Expanded(child: _firstPart),
-              Expanded(
-                child: LayoutBuilder(
-                  builder: (BuildContext context, BoxConstraints constraints) {
-                    final List<Widget> children = <Widget>[];
-                    final double maxWidth = constraints.maxWidth;
-                    final double width = (maxWidth / (_gridCount + 1)) * 2;
-                    for (int i = 0; i < _gridCount; i++) {
-                      final double offset = i * width / 2;
-                      final Widget grid = _buildGrid(width, offset, offset);
-                      children.add(grid);
-                    }
-                    for (int i = 0; i < _gridCount; i++) {
-                      final double offset = i * width / 2;
-                      final Widget mirrorGrid =
-                          _buildGrid(width, maxWidth - offset - width, offset);
-                      children.add(mirrorGrid);
-                    }
-                    return Stack(children: children);
-                  },
-                ),
-              ),
+              Expanded(child: _secondPart),
             ];
 
-            Widget body;
+            Axis direction;
             if (orientation == Orientation.portrait) {
-              body = Column(children: children);
+              direction = Axis.vertical;
             } else {
-              body = Row(children: children);
+              direction = Axis.horizontal;
             }
-            return body;
+
+            return Flex(
+              direction: direction,
+              children: children,
+            );
           },
         ),
       );
